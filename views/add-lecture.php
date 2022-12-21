060430dc5cd9cef075e408b1c8e3f93e0a8eaec6
Praise25 <anenepraise1@gmail.com>
2022-12-21 13:42:06 +0100

    fix: resolve issue link and title tags

--- views/add-lecture.php
+++ views/add-lecture.php
@@ -1,19 +1,13 @@
 <?php
 require_once('../lib/layout.php');
-$title = "<title>Lecture Attendance System</title>";
-$style = "<link rel='stylesheet' href='../static/add-lecture.css' />";
+$title = "Lecture Attendance System";
+$style = "../static/add-lecture.css";
 $lecturers = ["Lecturer 1", "Lecturer 2", "Lecturer 3", "Lecturer 4", "Lecturer 5"];
 $courses = ["Course 1", "Course 2", "Course 3", "Course 4", "Course 5"];
 ?>
 
 
-<header class="header">
-  <a href="admin.php" id="header-text">Admin Dashboard</a>
-
-  <div class="logout">
-    <a href="logout.php" class="btn btn-primary">Logout</a>
-  </div>
-</header>
+<?php include "layouts/header.php"; ?>
 
 <div class="container">
   <div class="row h-100">
