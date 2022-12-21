c2009d279711949428d4671bd60f60e27ee1e3f7
Praise25 <anenepraise1@gmail.com>
2022-12-21 21:02:37 +0100

    feat: implement lecturer dashboard

---
+++ views/lecturer/partials/header.php
@@ -0,0 +1,7 @@
+<header class="header">
+<a href="admin.php" id="header-text">Lecturer Dashboard</a>
+
+<div class="logout">
+  <a href="logout.php" class="btn btn-primary">Logout</a>
+</div>
+</header>
---
+++ views/lecturer/partials/side-bar.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="">View Courses</a></li>
+  <li><a href="">View Lectures</a></li>
+  <li><a href="">Create Reschedule Requests</a></li>
+</ul>
