cb5e11f5baf5890198508e561218e2bb1e4e9f46
Praise25 <anenepraise1@gmail.com>
2022-12-21 21:07:58 +0100

    feat: add sidebar link to generate student attendance link

--- views/lecturer/partials/side-bar.php
+++ views/lecturer/partials/side-bar.php
@@ -1,5 +1,6 @@
 <ul>
   <li><a href="">View Courses</a></li>
   <li><a href="">View Lectures</a></li>
+  <li><a href="">Generate Attendance Link</a></li>
   <li><a href="">Create Reschedule Requests</a></li>
 </ul>
