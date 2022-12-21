f3d183ab3ed4095cdb8f6c7e2a55ed56230384b5
John Afolayan <jnafolayan@gmail.com>
2022-12-21 15:42:24 +0100

    add admin and lecturer auth

---
+++ db.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+class DB
+{
+    public static $connection;
+
+    public static function getConnection()
+    {
+        $host = "localhost";
+        $user = "root";
+        $pass = "password";
+        $dbname = "jips";
+
+        if (!isset(self::$connection)) {
+            $conn = new mysqli($host, $user, $pass, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            self::$connection = $conn;
+        }
+
+        return self::$connection;
+    }
+}
+
+?>
