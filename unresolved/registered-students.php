25603165cadcdf46d1b388fd4e00bac3ab6181db
Praise25 <anenepraise1@gmail.com>
2022-12-21 22:42:01 +0100

    fix: resolve broken hyperlinks

--- unresolved/registered-students.php
+++ unresolved/registered-students.php
@@ -10,7 +10,7 @@
       integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
       crossorigin="anonymous"
     />
-    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="../static/index.css" />
     <title>Registered Students</title>
   </head>
   <body>
