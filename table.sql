635e0346b952aff220b1ba61cfafab53535331bf
John Afolayan <jnafolayan@gmail.com>
2022-12-23 10:15:50 +0100

    more module stuff

--- table.sql
+++ table.sql
@@ -60,8 +60,6 @@ CREATE TABLE IF NOT EXISTS `lecture` (
     endTime VARCHAR(10) NOT NULL,
     status VARCHAR(25) DEFAULT 'pending',
     isHOD BOOLEAN DEFAULT 0,
-    startedDateTime DATETIME(6) NULL,
-    endedDateTime DATETIME(6) NULL,
     createdAt DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
     updatedAt DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
     PRIMARY KEY(id),
@@ -71,6 +69,7 @@ CREATE TABLE IF NOT EXISTS `lecture` (
 CREATE TABLE IF NOT EXISTS `attendance` (
     lectureID INT UNSIGNED NOT NULL,
     matricNumber VARCHAR(25) NOT NULL,
+    createdAt DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
     PRIMARY KEY(lectureID, matricNumber),
     FOREIGN KEY(lectureID) REFERENCES lecture(id) ON DELETE CASCADE
 );
